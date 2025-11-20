27d0eb1c97d6410e5b26e7f2d7a6254a737b2e55
Rafi Fitzmaurice <rafi.fitzm@gmail.com>
2025-11-20 11:39:37 +0000

    Migrated movie model migration

--- db/migrate/20251120113534_create_movies.rb
+++ db/migrate/20251120113534_create_movies.rb
@@ -1,10 +1,10 @@
 class CreateMovies < ActiveRecord::Migration[7.1]
   def change
     create_table :movies do |t|
-      t.string :title
-      t.string :overview
+      t.string :title, null: false
+      t.string :overview, null: false
       t.string :poster_url
-      t.decimal :rating
+      t.decimal :rating, null: false
 
       t.timestamps
     end
