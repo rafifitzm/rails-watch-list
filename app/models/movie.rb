724851119bee0a523d50d0c7cc6886051ae1b796
Rafi Fitzmaurice <rafi.fitzm@gmail.com>
2025-11-20 12:38:39 +0000

    Movie should not be able to destroy self if has bookmark children

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,8 @@ class Movie < ApplicationRecord
   validates :title, :overview, presence: true
   validates :title, uniqueness: true
 
-  has_many :bookmarks, dependent: :destroy
+  # Should not be able to destroy self if has bookmarks children (via an ActiveRecord::InvalidForeignKey error from bookmarks table)
+  has_many :bookmarks
+
   has_many :lists, through: :bookmarks
 end
